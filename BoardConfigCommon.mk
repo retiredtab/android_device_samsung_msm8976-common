687691a81e028d002919f9f1d069b5b0c9d72512
retiredtab-gerrit <clrh1tablet@gmail.com>
2023-01-04 19:29:16 -0700

    msm8976-common: Let's go selinux enforcing.
    Just comment it out so we can revert for future builds.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,7 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.wificountrycode=00 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
